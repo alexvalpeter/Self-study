e5852d0a5c6ca5435cf73090f3e3731ce5e6cbd2
Alex <alexvalpeter@gmail.com>
2022-03-12 05:53:05 -0500

    Link JS app and add listener on theme toggle

--- Frontend Mentor/Calculator App/app.js
+++ Frontend Mentor/Calculator App/app.js
@@ -3,7 +3,7 @@ const logo = document.querySelector(".logo");
 const themeToggle = document.querySelector("#theme-toggle");
 
 /**
- * Changes visual theme and color scheme of page
+ * Changes visual theme and color scheme of page according to user input
  *
  * @param {number} theme Number corresponding to user-chosen theme
  */
@@ -11,6 +11,6 @@ function switchTheme(theme){
 
 }
 
-themeToggle.addEventListener("input", => {
+themeToggle.addEventListener("input", () => {
     console.log(themeToggle.value);
-})
+});
