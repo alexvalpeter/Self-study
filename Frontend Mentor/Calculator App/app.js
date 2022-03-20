6688880dc5dad93668c41c5370a14deffd3e7201
Alex <alexvalpeter@gmail.com>
2022-03-20 02:53:28 -0400

    Impletement basic delete, append functions, refactor updateDisplay function

--- Frontend Mentor/Calculator App/app.js
+++ Frontend Mentor/Calculator App/app.js
@@ -1,17 +1,63 @@
 const body = document.querySelector("body");
 const logo = document.querySelector(".logo");
 const themeToggle = document.querySelector("#theme-toggle");
+const display = document.querySelector("#display-box");
+let displayText = "";
 
 /**
  * Changes visual theme and color scheme of page according to user input
  *
  * @param {number} theme Number corresponding to user-chosen theme
  */
-function switchTheme(theme){
-    body.className = "theme-" + theme;
-
-}
+function switchTheme(themeNumber){
+    body.className = "theme-" + themeNumber;
+};
 
 themeToggle.addEventListener("input", function(){ 
     switchTheme(themeToggle.value);
 });
+
+document.querySelectorAll(".num").forEach( (element) => {
+    element.addEventListener("click", () => {
+        if (displayText == "0"){
+            displayText = element.innerHTML;
+        } else {
+            appendDigit(element.innerHTML);
+        };
+
+        updateDisplay();
+    });
+}); 
+
+document.querySelector(".reset").addEventListener("click", () => {
+    clearDisplay();
+});
+
+document.querySelector(".del").addEventListener("click", () => {
+    delDigit();
+});
+
+function appendDigit(newDigit){
+    displayText += newDigit;
+};
+
+function updateDisplay(){
+   // displayText = displayText.concat(newData);
+    display.innerHTML = displayText;
+};
+
+function delDigit (){
+    if (displayText.length == 1){
+        displayText = "0";
+    } else {
+        displayText = displayText.substring(0, displayText.length-1);
+    };
+        
+    updateDisplay();
+};
+
+function clearDisplay () {
+    displayText = "0"
+    display.innerHTML = displayText;
+};
+
