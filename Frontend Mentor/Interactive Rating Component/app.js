8c2e170f1aafbcafa44b9ab79d75e9064557ba88
Alex <alexvalpeter@gmail.com>
2023-04-23 22:18:31 -0400

    Desktop/tablet styles

--- Frontend Mentor/Interactive Rating Component/app.js
+++ Frontend Mentor/Interactive Rating Component/app.js
@@ -5,12 +5,21 @@ let rating;
 
 const ratingForm = document.querySelector("#rating-form");
 
+/*
+* Listen for submit event from HTML form
+* Store submitted value of rating
+*/
 ratingForm.addEventListener("submit", event => {
     event.preventDefault();
     rating = document.querySelector('input[name="rating"]:checked').value;
     switchState();
 })
 
+/*
+* Hide rating state
+* Insert submitted rating number from user form
+* Display "thank you" state
+*/
 function switchState() {
     ratingState.style.display = "none"
     document.querySelector(".selection-text span").innerHTML = rating;
