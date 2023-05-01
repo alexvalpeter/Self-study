dd9bec078345577cbb968cef1ed026613977c8cb
Alex <alexvalpeter@gmail.com>
2023-04-30 23:06:57 -0400

    Add custom error message on unfocus

--- Frontend Mentor/Intro Component/app.js
+++ Frontend Mentor/Intro Component/app.js
@@ -3,8 +3,10 @@ document.querySelectorAll("input:not([type=submit])").forEach((element) => {
     element.addEventListener("focusout", (event) => {
         if(!element.checkValidity()) {
             element.classList.add("error");
+            document.querySelector("#" + element.id + "~.error-msg").style.display = "block";
         } else {
             element.classList.remove("error");
+            document.querySelector("#" + element.id + "~.error-msg").style.display = "none";
         }
 
         if(element.id == "email"){
